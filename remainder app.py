0b33695973da514ab471cef0b1b87bda5ebb1349
Rishi Ratan Pandey <83863516+Pyton-Projects@users.noreply.github.com>
2021-05-15 20:12:10 +0530

    Update remainder app.py

--- remainder app.py
+++ remainder app.py
@@ -7,7 +7,9 @@ import time as ee
 from ttkthemes import ThemedTk,THEMES
 import datetime
 import sys
-facts=['The world’s oldest toy is a stick',]
+import random
+import winsound
+facts='The world’s oldest toy is a stick','Moon flowers actually bloom in response to the moon','The biggest Dalmatian litter ever was 18 puppies','Sea otters hold hands while they sleep','The largest sand castle in the world measured 54 feet high','There are over 10 holidays that celebrate chocolate','Chocolate is (kinda) a fruit','The voices of Mickey and Minnie Mouse got married in real life','Lazy people actually think more','Fall leaf colors are present year-round','Two Buck Chuck once won a wine competition','The way you eat Oreos says something about your personality','The Queen drinks champagne almost every day','The moon has its own time zones','Americans eat 100 acres of pizza a day','Smiling is actually contagious','A team of six women programmed the first digital computer','Baby elephants suck their trunks for comfort','''There's a 107-acre forest made up of a single tree''','Tomatoes are both a fruit and a vegetable','''There's an official rock paper scissors league'''
 def x():
 	question=messagebox.askquestion('Message','Are You Sure To Quit? Timer Will Be Disabled If You Quit')
 	if question=='yes':
@@ -15,30 +17,28 @@ def x():
 	if question!='no':
 		None
 def notification_():
-	
+
 	try:
 		notification.notify(
             	title = "Your Remainder!",
-            	message=F"""{task_value_}
-Do You Know? 
-{str(facts)} Thats a Fact!""")# create button called increase knowledge mode is on or off
+            	message=F"""{task_value_}Do You Know? 
+{random.choice(facts)} Thats a Fact!""")
+		frequency = 1000
+		duration = 2000 
+		winsound.Beep(frequency, duration)
 	except Exception:
 		messagebox.showinfo('Message','Please Set A Task Then Cilck On Set Task Button')
 def timer():
 	if len(task.get("1.0", "end-1c")) ==0:
 		messagebox.showinfo('Message','Please Set A Task Then Cilck On Set Task Button')
-	else:
+	
+	if len(task.get("1.0", "end-1c")) !=0:	
 		formula_to_find_seconds=sec_value.get()*1000
 		formula_to_find_minutes=min_value.get()*60000
 		formula_to_find_hours=hour_value.get()*3600000
 		time=formula_to_find_seconds+formula_to_find_minutes+formula_to_find_hours
 		root.after(time,notification_)
-		time_=ee.strftime('%H%M%S')
-		time__=list(time_)
-		for a in time__:
-			print(a,end='')# how to convert millseconds to seconds minutes or hour
-		NU='btu'
-		messagebox.showinfo('Message',f'The Time Is Set At {time_} You Will Get A Notifaction At {int(a)+formula_to_find_seconds+formula_to_find_minutes+formula_to_find_hours}')
+		messagebox.showinfo('Message','Timer Set Suceesfully!')
 def task_value():
 	global task_value_
 	task_value_=task.get(1.0,END)
@@ -54,40 +54,31 @@ root.minsize(900,400)
 min_value=IntVar()
 sec_value=IntVar()
 hour_value=IntVar()
+line.pack()
 task=Text(root,width=15,height=5)
-task.place(x=750+19+3)
+task.place(x=365,y=250)
 root.title('Remainder Application For Windows')
 heading_label=ttk.Label(root,text='Desktop Remainder App',font=('Times',15))
 heading_label.pack()
 start=ttk.Spinbox(root,from_=0,to=23,width=3,textvariable=hour_value,font=Font(family='times',size=15))
-start.place(x=150,y=65)
+start.place(x=400,y=65)
 start['state']='readonly'
 hour=ttk.Label(root,text='Hour',font=('Times',13))# go and find some facts
-hour.place(x=95,y=65)
+hour.place(x=340,y=65)
 min_=ttk.Label(root,text='Minutes',font=('Times',13))
-min_.place(x=85,y=100)
+min_.place(x=330,y=100)
 sec_=ttk.Label(root,text='Seconds',font=('Times',13))
-sec_.place(x=85,y=139)
-Task_Identity=ttk.Label(root,text='Put Your Task Down Below When Time Hits The Task Will Show In The Notifaction')
-Task_Identity.pack()
+sec_.place(x=330,y=139)
+Task_Identity=ttk.Label(root,text='Put Your Task Down Below When Time Hits The Task Will Show In The Notifaction',font=('Times',11))
+Task_Identity.place(x=165,y=220)
 start_min=ttk.Spinbox(root,from_=0,to=59,width=3,textvariable=min_value,font=Font(root,family='times',size=15))
-start_min.place(x=150,y=100)
+start_min.place(x=400,y=100)
 start_min['state']='readonly'
 start_sec=ttk.Spinbox(root,from_=0,to=59,width=3,textvariable=sec_value,font=Font(root,family='times',size=15))
-start_sec.place(x=150,y=140)
+start_sec.place(x=400,y=140)
 start_sec['state']='readonly'
 set_=ttk.Button(root,text='Set Timer',command=timer)
-set_.place(x=135,y=175)
+set_.place(x=385,y=180)
 set_task=ttk.Button(root,text='Set Task',command=task_value)
-set_task.place(x=750+19+3+6+3+3+6+3+3,y=50+12+19+5,)
+set_task.place(x=385,y=340,)
 root.mainloop()
-# like time now 12:00:1 15 minutes of timer 12:15:00
-# 12:00 to 12:15 
-# ideas in diary
-# notifaction type
-# set task to do progject
-# a software that can get the color value form a int
-# complete image commpresser.
-# put some facts in notifaction and create option to get notifaction in knowlegable form or not knowlegable form.
-# WHY SUMBLIME TEXT SHORCUT RN SHORCUT IS Ctrl+B?
-# answer-->Ctrl+c copy code from stackoverflow # ctrl+v paste it to text editor then press ctrl+b to execute it...!
