7a34dde8bdeca8018890ec918b78725753c735ce
Rishi Ratan Pandey <83863516+Pyton-Projects@users.noreply.github.com>
2021-05-19 12:44:50 +0530

    Update remainder app.py

--- remainder app.py
+++ remainder app.py
@@ -11,40 +11,40 @@ import random
 import winsound
 facts='The world’s oldest toy is a stick','Moon flowers actually bloom in response to the moon','The biggest Dalmatian litter ever was 18 puppies','Sea otters hold hands while they sleep','The largest sand castle in the world measured 54 feet high','There are over 10 holidays that celebrate chocolate','Chocolate is (kinda) a fruit','The voices of Mickey and Minnie Mouse got married in real life','Lazy people actually think more','Fall leaf colors are present year-round','Two Buck Chuck once won a wine competition','The way you eat Oreos says something about your personality','The Queen drinks champagne almost every day','The moon has its own time zones','Americans eat 100 acres of pizza a day','Smiling is actually contagious','A team of six women programmed the first digital computer','Baby elephants suck their trunks for comfort','''There's a 107-acre forest made up of a single tree''','Tomatoes are both a fruit and a vegetable','''There's an official rock paper scissors league'''
 def x():
-	question=messagebox.askquestion('Message','Are You Sure To Quit? Timer Will Be Disabled If You Quit')
-	if question=='yes':
-		sys.exit()
-	if question!='no':
-		None
+    question=messagebox.askquestion('Message','Are You Sure To Quit? Timer Will Be Disabled If You Quit')
+    if question=='yes':
+        sys.exit()
+    if question!='no':
+        None
 def notification_():
 
-	try:
-		notification.notify(
-            	title = "Your Remainder!",
-            	message=F"""{task_value_}Do You Know? 
+    try:
+        notification.notify(
+                title = "Your Remainder!",
+                message=F"""{task_value_}Do You Know? 
 {random.choice(facts)} Thats a Fact!""")
-		frequency = 2000
-		duration = 2000 
-		winsound.Beep(frequency, duration)
-	except Exception:
-		messagebox.showinfo('Message','Please Set A Task Then Cilck On Set Task Button')
+        frequency = 2000
+        duration = 2000 
+        winsound.Beep(frequency, duration)
+    except Exception:
+        messagebox.showinfo('Message','Please Set A Task Then Cilck On Set Task Button')
 def timer():
-	if len(task.get("1.0", "end-1c")) ==0:
-		messagebox.showinfo('Message','Please Set A Task Then Cilck On Set Task Button')
-	else:
-		messagebox.showinfo('Message','Timer Set Suceesfully!')	
-		formula_to_find_seconds=sec_value.get()*1000
-		formula_to_find_minutes=min_value.get()*60000
-		formula_to_find_hours=hour_value.get()*3600000
-		time=formula_to_find_seconds+formula_to_find_minutes+formula_to_find_hours
-		root.after(time,notification_)
+    if len(task.get("1.0", "end-1c")) ==0:
+        messagebox.showinfo('Message','Please Set A Task Then Cilck On Set Task Button')
+    else:
+        messagebox.showinfo('Message','Timer Set Suceesfully!') 
+        formula_to_find_seconds=sec_value.get()*1000
+        formula_to_find_minutes=min_value.get()*60000
+        formula_to_find_hours=hour_value.get()*3600000
+        time=formula_to_find_seconds+formula_to_find_minutes+formula_to_find_hours
+        root.after(time,notification_)
 def task_value():
-	global task_value_
-	task_value_=task.get(1.0,END)
-	if len(task.get("1.0", "end-1c")) == 0:# This Logic Came From https://stackoverflow.com/questions/38539617/tkinter-check-if-text-widget-is-empty
-		question=messagebox.showinfo('Message','Please Put Something For Your Task!')
-	if len(task.get("1.0", "end-1c")) != 0:
-		messagebox.showinfo('Message','Task Set Suceesfully! Now Go And Set Your Timer! ')# create a menu 
+    global task_value_
+    task_value_=task.get(1.0,END)
+    if len(task.get("1.0", "end-1c")) == 0:# This Logic Came From https://stackoverflow.com/questions/38539617/tkinter-check-if-text-widget-is-empty
+        question=messagebox.showinfo('Message','Please Put Something For Your Task!')
+    if len(task.get("1.0", "end-1c")) != 0:
+        messagebox.showinfo('Message','Task Set Suceesfully! Now Go And Set Your Timer! ')# create a menu 
 root=ThemedTk(themebg=True)
 root.set_theme('arc')
 root.protocol('WM_DELETE_WINDOW',x)
