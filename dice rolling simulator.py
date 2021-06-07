4278b17d8d9710b784020f7df78dfe09a388b558
Rishi Ratan Pandey <83863516+Pyton-Projects@users.noreply.github.com>
2021-06-07 14:06:17 +0530

    Update dice rolling simulator.py

--- dice rolling simulator.py
+++ dice rolling simulator.py
@@ -1,5 +1,8 @@
 from tkinter import*
+from ttkthemes import*
+from tkinter import ttk
 from tkinter import messagebox
+from tkinter.font import Font
 import random # a meme idea--> kids uses-ide legend uses-notepad
 root=Tk() # create a normal dice.
 a=10
@@ -21,7 +24,9 @@ dice_l_2=Label(root,font=('helvetica',260))
 dice_l_4=Label(root,font=('helvetica',45))
 or_label=Label(root,font=('helvetica',45))
 money=2000
-rs_Label=Label(root,text=f'Balance-{money}$',font=('Courier',30))
+text_mon_dat=StringVar()
+text_mon_dat.set(f'Balance-{money}$')
+rs_Label=Label(root,text=f'Balance-{money}$',font=('Courier',30),textvariable=text_mon_dat)
 rs_Label.place(x=1,y=1)
 about_pro=Button(root,text='\u24D8',font=(5),border=0)
 about_pro.place(x=600-300,y=100)
@@ -31,7 +36,7 @@ about_pro_max_ultra=Button(root,text='\u24D8',border=0,font=(5))
 about_pro_max_ultra.place(x=600,y=100)
 def about_pro_():
 	from tkinter import messagebox
-	messagebox.showinfo('Message','This Dice Has More Chances To Get Four. And Your 2 Times Point Will Increase')
+	messagebox.showinfo('Message','This Dice Has More Chances To Get Four. And Your 2 Times Point Will Increase And Also Your Money Will Increase 2 Times')
 def about_pro_max_():
 	from tkinter import messagebox
 	messagebox.showinfo('Message','This Dice Has More Chances To Get Five. And Your 4 Times Point Will Increase')
@@ -50,25 +55,39 @@ chances1=6
 def chances():
 	global chances_label
 	global val
+	global chances1
+	global money
+	text_mon_dat.set(f'Balance-{money}$')
+
+	dat_two.set(f'Balance-{money}$')
+
 	val=StringVar()
 	val.set(f'Chances Left:{chances1}')
 	chances_label=Label(top_level,text=f'Chances Left:{chances1}',textvariable=val,font=('Courier',20))
 	chances_label.place(x=665,y=40)
 	if chances1==0:
-		ques_1=messagebox.askquestion('Info','Chances Finised!! Do You Want To Buy? More Chances?')
+		dat_two.set(f'Balance-{money}$')
+		text_mon_dat.set(f'Balance-{money}$')
+
+		ques_1=messagebox.askquestion('Info','Chances Finised!!!! Do You Want To Buy More Chances?')
 		# show shop if answer is yes else:
 		if ques_1=='yes':
 			# show shop
-			pass
+			store_to_buy_more_chances()
 		else:
+
 			messagebox.showinfo('(:',f'Your Luck Scored:{point_dat.get()[::-1][0:2:][::-1]}')
 			Start_The_Roll.config(state=DISABLED)
+			chances1=6
+
+			
 			root.deiconify()
+			top_level.destroy()
 
+			
 
-		# show spinbox chances
+		# show spinbox to buy chances
   
-import time
 def main_roll():
 	global random_
 	this_dice_has_MORE_four='\u2683','\u2683','\u2683','\u2683','\u2680','\u2681','\u2682','\u2684','\u2685'
@@ -77,39 +96,107 @@ def main_roll():
 	print(random_)
 	global point
 	global chances1
+	global money
+	global money
+	dat_two.set(f'Balance-{money}$')
+
 	if (random_)=='\u2680':
-		point-=1
+		point+=2 # add money also
 		chances1-=1
 		point_dat.set(f'Point:{point}')
+		money+=10
+		
+		print(money)
+		dat_two.set(f'Balance-{money}$')
+
+		text_mon_dat.set(f'Balance-{money}$')
 		chances()
+
+
+
 	elif (random_)=='\u2681':
-		point+=2
+		point+=4
 		chances1-=1		
 		point_dat.set(f'Point:{point}')
+		money+=40
+		print(money)
+		dat_two.set(f'Balance-{money}$')
+
+		text_mon_dat.set(f'Balance-{money}$')
 		chances()
+
 	elif (random_)=='\u2682':
-		point+=4
-		chances1-=1
-		point_dat.set(f'Point:{point}')
-		chances()
-	elif (random_)=='\u2683':
 		point+=6
 		chances1-=1
 		point_dat.set(f'Point:{point}')
+		money+=60
+
+		print(money)
+		dat_two.set(f'Balance-{money}$')
+
+		text_mon_dat.set(f'Balance-{money}$')
 		chances()
-	if (random_)=='\u2684':
+
+	elif (random_)=='\u2683':
 		point+=8
 		chances1-=1
 		point_dat.set(f'Point:{point}')
+		money+=80
+		print(money)
+		dat_two.set(f'Balance-{money}$')
+		text_mon_dat.set(f'Balance-{money}$')
 		chances()
-	elif (random_)=='\u2685':
+
+	if (random_)=='\u2684':
 		point+=10
 		chances1-=1
 		point_dat.set(f'Point:{point}')
+		money+=100
+		print(money)
+		dat_two.set(f'Balance-{money}$')
+		text_mon_dat.set(f'Balance-{money}$')
 		chances()
+
+	elif (random_)=='\u2685':
+		point+=12
+		chances1-=1
+		point_dat.set(f'Point:{point}')
+		money+=120
+		print(money)
+		dat_two.set(f'Balance-{money}$')
+		text_mon_dat.set(f'Balance-{money}$')
+		chances()
+
+	else:
+		dat_two.set(f'Balance-{money}$')
+		text_mon_dat.set(f'Balance-{money}$')
+
+	dat_two.set(f'Balance-{money}$')
+
+
+# 	heiiii=open('score.py','w')
+# 	dat=heiiii.write(f'''points=[{point},{point}].sort()
+# print(points)''')
+# 	# create hight socre option
+
+# 	print(open('score.py','r').read())
+# 	dat_two.set(f'Balance-{money}$')
 def store_to_buy_more_chances():
 	# tommrow code shop
-	pass
+	# spinbox to buy how many
+	Strore=ThemedTk(themebg=True)
+	Strore.set_theme('arc')
+	indie=tkk.Label(root,text='How Many Chances? :')
+	head=ttk.Label(Strore,text='Store',font=('Courier',30,'bold'))
+	number_of_chaces=ttk.Spinbox(Strore,from_=0,to=24,width=3,font=Font(family='times',size=15))
+	head.pack()
+	indie.pack(  )
+	number_of_chaces['state']='readonly'
+	number_of_chaces.pack()
+
+
+
+
 def main_pro():
 	root.iconify()
 	global  dice_l_3
@@ -118,6 +205,7 @@ def main_pro():
 	global point
 	global top_level
 	global Start_The_Roll
+	global dat_two
 	top_level=Toplevel()
 	dice_l_3=Label(top_level,text='\u2683',font=('helvetica',260))
 	point=1
@@ -127,21 +215,24 @@ def main_pro():
 	top_level.minsize(900,450)
 	top_level.title('Dice Simulator')
 	Start_The_Roll=Button(top_level,text='Roll The Dice!!',command=main_roll)
-	rs_Label=Label(top_level,text=f'Balance-{minus_value}$',font=('Courier',20))
+	dat_two=StringVar()
+	rs_Label=Label(top_level,text=f'Balance-{money}$',font=('Courier',20),textvariable=dat_two)
+	dat_two.set(f'Balance-{money}$')
 	rs_Label.place(x=1,y=1)
 	Point=Label(top_level,text=f'Point:{point}',font=('Courier',20),textvariable=point_dat)
-	point_dat.set(f'Point:{point}')
+	point_dat.set(f'Point: {point}')
 	Point.place(x=750,y=1)
-	Start_The_Roll.place(x=380,y=300)
-def minus_for_prodice():
-	if money>=700:
+	Start_The_Roll.place(x=380,y=310)
+	dat_two.set(f'Balance-{money}$')
+	text_mon_dat.set(f'Balance-{money}$')
 
+def minus_for_prodice():
+	global money
+	if money>=700:
 		from tkinter import messagebox
-		global minus_value
-		minus_value=money-700
-		print(minus_value)
-		rs_Label.config(text=f'Balance-{minus_value}$')
-		messagebox.showinfo('Info','Loading Main Scene.')
+		money-=700
+		rs_Label.config(text=f'Balance:{money}$')
+		messagebox.showinfo('Info','Loading Main Scene..')
 		main_pro()
 	else:
 		radio_6_get.set(0)
@@ -150,12 +241,12 @@ def minus_for_prodice():
 def minus_for_promaxdice():
 	if money>=999:
 
-		global minus_value_
-		minus_value_=money-999
-		print(minus_value_)
-		rs_Label.config(text=f'Balance-{minus_value_}$')
+		global money_
+		money_=money-999
+		print(money_)
+		rs_Label.config(text=f'Balance-{money_}$')
 		from tkinter import messagebox
-		messagebox.showinfo('Info','Loading Main Scene.')
+		messagebox.showinfo('Info','Loading Main Scene..')
 	else:
 		radio_6_get.set(0)
 		from tkinter import messagebox
@@ -163,12 +254,12 @@ def minus_for_promaxdice():
 def minus_for_ultrapromaxdice():
 	if money>=2000:
 
-		global minus_value_Bts
-		minus_value_Bts=money-1999
-		print(minus_value_Bts)
-		rs_Label.config(text=f'Balance-{minus_value_}$')
+		global money_Bts
+		money_Bts=money-1999
+		print(money_Bts)
+		rs_Label.config(text=f'Balance-{money_}$')
 		from tkinter import messagebox
-		messagebox.showinfo('Info','Loading Main Scene.')
+		messagebox.showinfo('Info','Loading Main Scene..')
 	else:
 		radio_6_get.set(0)
 		from tkinter import messagebox
@@ -215,6 +306,5 @@ def roll_dice():
 	dice_l_3_1.place(x=300,y=1)
 b1=Button(root,text='Buy This One And Go!',command=get_value)
 roll_dice()
-b1.pack()
-b1.place(x=350,y=425)
+b1.place(x=345,y=425)
 root.mainloop()
