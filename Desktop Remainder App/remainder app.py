1bda5f6700ab69675dc029dddc13b45f4660fef0
Rishi Ratan Pandey <83863516+Rishi-Ratan-Pandey@users.noreply.github.com>
2021-06-23 21:25:07 +0530

    Update remainder app.py

--- Desktop Remainder App/remainder app.py
+++ Desktop Remainder App/remainder app.py
@@ -16,9 +16,8 @@ min_value=IntVar()# create menus in remainder app and deice roling simulator
 sec_value=IntVar()# ansea lib for sync audio to text!
 hour_value=IntVar()#create stickman animation
 get__=StringVar()
-#bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11bts&&txt11
-# bts&&txt11bts&&txt11bts&&txt11
-# bts&&txt11bts&&txt11bts&&txt11
+check_11=False
+
 def which_alert():
 	d = Dialog(None, {'title': 'Question',
                       'text':'Which Alert Do You Want? You Will Remind For Your Task Through Your Selected Alert',
@@ -30,59 +29,59 @@ def which_alert():
 	set_.config(command=info1)
 	global Selected_Val
 	Selected_Val=d.num
-	print(Selected_Val)
+def c():
+	notification.notify(title='Canceled..!',message='')
+check_22=False#fix bugs.
 def ask_phone_number():
+	global check_22
 	global phone_number
 	phone_number=simpledialog.askstring(title='Phone Number',prompt='Enter Your Phone Number (With Contury Code Incuded)')
-	
+	check_22=True
+	t_1=ttk.Label(root,text=datetime.now().strftime('%r'))#HIDE TH AUTH_TOKEN
+	t_1.place(x=570,y=80)
+	cancel.place(x=665,y=80)
+	t=ttk.Label(root,text=(datetime.now()+timedelta(seconds=sec_value.get(),minutes=min_value.get(),hours=hour_value.get())).strftime('%r'))
+	t.place(x=825,y=80)
 def sms_alert():
-	
+	global check_11
 	pro_1.stop()
 	set_.config(command=Remainder)
-	
+	auccont_Id=os.environ.get('MY_SID')
+	auth_token=os.environ.get('MY_AUTH_TOKEN')
 	c=Client(auccont_Id,auth_token)
-	# try:
-	if check==False:
-			c.messages.create(body=F"Hey! user You Need To Do {task_value_} Reply Yes To Confrom That You Reviced The Alert!",from_='+19563985957',to=phone_number)
-	if check==True:
-			c.messages.create(body=F"Hey! user You Need To Do {update_task} Reply Yes To Confrom That You Reviced The Alert!",from_='+19563985957',to=phone_number)
-	# except:
-	# 	messagebox.showinfo('Info','Check Your Network Connection. Or Check The Phone Number You Entered (Your Phone Number-{number})'.format(number=phone_number))
+	if check_11==False:
+		try:
+			if check==False:
+				c.messages.create(body=F"Hey! user You Need To Do {task_value_} At This Time Reply Yes To Confrom That You Reviced The Alert!",from_='+19563985957',to=phone_number)
+			if check==True:
+				c.messages.create(body=F"Hey! user You Need To Do {update_task} AT This Time Reply Yes To Confrom That You Reviced The Alert!",from_='+19563985957',to=phone_number)
 		
-
-def phone_alert():#password:
-	
-
+		except:
+			messagebox.showinfo('Info','Check Your Network Connection. Or Check The Phone Number You Entered (Your Phone Number-{number})'.format(number=phone_number))
+			
+	if check_11==True:
+		pass
+	# check anonther reaminder app,correct sppelings,rename repo
+		
+def phone_alert():
 	pro_1.stop()
 	set_.config(command=Remainder)
-	"""Algorithm:
-		ask a phone number
-		wait till the timer hits the time
-		if timer hits the time call the user"""
-		
-
-	c=Client(auccont_Id,auth_token)#say the task
+	auccont_Id=os.environ.get('MY_SID')
+	auth_token=os.environ.get('MY_AUTH_TOKEN')
+	print(auth_token,auccont_Id)
+	c=Client(str(auccont_Id),str(auth_token))#say the task
 	try:
 		c.calls.create(from_='+19563985957',url='https://demo.twilio.com/docs/voice.xml',to=phone_number)
 	except:
 		messagebox.showinfo('Info','Check Your Network Connection Or Check The Phone Number You Entered (Your Phone Number-{number})'.format(number=phone_number))
-
-
+		
 def new_remainder():
 	pass
-			# CREATE bUTTON fOR  FOR CREATING NEW REMAINDER
-		# use elif when there is a chance to get both satement true
-
-		# a softwarre that can short english sentence!
-		# use cd to switch in different folders use ; to add diffrent paths in one varible!!,a website that can load webpages faster!!!
-		# create bakend of alerts
-		#ask a question called how to install multiple packges at once in pip/python
-
 def x():
 	question=messagebox.askquestion('Message','Are You Sure To Quit? Remainder Will Be Disabled If You Quit')
 	if question=='yes':
 		exit()
-	if question!='no':
+	if question=='no':
 		None
 def notification_():
 	set_.config(command=Remainder)
@@ -108,7 +107,6 @@ def info():
 	messagebox.showinfo('Message','Please Set A Task From Down Below Then Cilck On Set Remaind Button')
 def main_pros():
 	l.place(x=660,y=30)
-
 	sez=(min_value.get())
 	for_mula=sez*600# number genereter
 	sez_1=(sec_value.get())
@@ -118,19 +116,36 @@ def main_pros():
 	pro_1['mode']='determinate'# create top level a python program that can short english sentence
 	pro_1['value']=1000
 	pro_1.start(for_mula+for_mula_1+for_mula_2)
-	root.update()
+	root.update()#clean github repotries
 
 def info1():
 	messagebox.showinfo('Message','Another Remind Is Running Do You Want To Set Another Remainder? If Yes Then Go To File Menu Then Click On New Remainder Option.')
 get__.set('Intaial value')
+def cancel():
+	pro_1.stop()
+	global check_11
+	check_11=True
+	if Selected_Val==0:
+		if check_22==True:
+			pass
+		if check_22==False:
+			sms_alert()
+		else:
+			pass
+	if Selected_Val==1:
+		phone_alert()
+	if Selected_Val==2:
+		notification_()
 def Remainder():
-	print(get__.get())
+
 	if sec_value.get()==0 and min_value.get()==0 and hour_value.get()==0:
 			messagebox.showinfo('Message','Please Add More Time!')	
 	#  a most used word in a programmer lif is ;
 	else:
 		global time# how to give a python to give 
 		global updated
+		global check_11
+		print(check_11)
 		print('''Remaind Is Running.... Please Don't Quit The Application. ''')
 		formula_to_find_seconds=sec_value.get()*1000
 		formula_to_find_minutes=min_value.get()*60000
@@ -138,14 +153,32 @@ def Remainder():
 		time=formula_to_find_seconds+formula_to_find_minutes+formula_to_find_hours
 		which_alert()
 		if Selected_Val==0:
+			check_11=False
+			t_1=ttk.Label(root,text=datetime.now().strftime('%r'))#HIDE TH AUTH_TOKEN
+			t_1.place(x=570,y=80)
+			cancel.place(x=665,y=80)
+			t=ttk.Label(root,text=(datetime.now()+timedelta(seconds=sec_value.get(),minutes=min_value.get(),hours=hour_value.get())).strftime('%r'))
+			t.place(x=825,y=80)
 			ask_phone_number()
 			root.after(time,sms_alert)
-			main_pros()#add memus!
+			main_pros()
 		if Selected_Val==1:
+			check_11=False
+			t_1=ttk.Label(root,text=datetime.now().strftime('%r'))#HIDE TH AUTH_TOKEN
+			t_1.place(x=570,y=80)
+			cancel.place(x=665,y=80)
+			t=ttk.Label(root,text=(datetime.now()+timedelta(seconds=sec_value.get(),minutes=min_value.get(),hours=hour_value.get())).strftime('%r'))
+			t.place(x=825,y=80)
 			ask_phone_number()
 			root.after(time,phone_alert)
 			main_pros()
 		if Selected_Val==2:
+			t_1=ttk.Label(root,text=datetime.now().strftime('%r'))#HIDE TH AUTH_TOKEN
+			cancel.place(x=665,y=80)
+			print(time)
+			t=ttk.Label(root,text=(datetime.now()+timedelta(seconds=sec_value.get(),minutes=min_value.get(),hours=hour_value.get())).strftime('%r'))
+			t_1.place(x=570,y=80)
+			t.place(x=825,y=80)
 			root.after(time,notification_)
 			main_pros()
 def task_value():
@@ -162,22 +195,19 @@ def task_value():
 	else:
 		messagebox.showinfo('Message','Remind Set Successfully! Now Go And Set Your Remainder')
 		set_.config(command=Remainder)
-		update.place(x=500,y=340)
+		update.place(x=500,y=340)#stopwatch
 def update_task():
 	global check
 	global new_task
 	new_task=task.get(1.0,END)
 	check=True
 	messagebox.showinfo('Info','Task Updated!')
+
 root.protocol('WM_DELETE_WINDOW',x)
 pro_1=Progressbar(root,length=250)
 pro_1.place(x=600,y=60)
 dat=StringVar()
-then=(datetime.now()+timedelta(seconds=sec_value.get(),minutes=min_value.get(),hours=hour_value.get())).strftime('%r')
-l_1=ttk.Label(root,text=then,textvariable=dat)
-l_1.place(x=530,y=80)
-dat.set(then)
-
+print(sec_value)
 l=ttk.Label(root,text='Progress Of Your Remind',font=('Time',10))
 l.place(x=660,y=30)
 # use elf when thre is a chance to get both satement true
@@ -206,7 +236,7 @@ hour.place(x=340,y=65)
 min_=ttk.Label(root,text='Minutes',font=('Times',13))
 min_.place(x=330,y=100)
 sec_=ttk.Label(root,text='Seconds',font=('Times',13))
-sec_.place(x=330,y=139)
+sec_.place(x=330,y=139)# today sutdy-goal!!
 Task_Identity=ttk.Label(root,text='What shall I remind you about? Write Down Below',font=('Times',11))
 Task_Identity.place(x=300,y=220)
 start_min=ttk.Spinbox(root,from_=0,to=59,width=3,textvariable=min_value,font=Font(root,family='times',size=15))
@@ -220,7 +250,7 @@ set_.place(x=375,y=180)
 set_task=ttk.Button(root,text='Set Remind',command=task_value)
 set_task.place(x=385,y=340,)
 update=ttk.Button(root,text='Click Me For Update Your Old Remind!',command=update_task)
-
+cancel=ttk.Button(root,text='Cancel The Remaind',command=cancel)# check speelings,study today.
 mainloop()
 # End!
 # CREATE bUTTON fOR  FOR CREATING NEW REMAINDER
